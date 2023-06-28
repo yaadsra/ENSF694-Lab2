61d50cc2dace82ebd02047df923b980b29047305
Yaad Sra <132399828+yaadsra@users.noreply.github.com>
2023-06-28 16:11:55 -0600

    updated java file

--- LAB2694/src/lab2694.java
+++ LAB2694/src/lab2694.java
@@ -3,16 +3,10 @@
 	import java.util.Scanner;
 
 	/**
-	 * This class implements linear search and interpolation search algorithms to search for a key in an integer array.
+	 * This class implements linear search, improved linear search, and interpolation search algorithms to search for a key in an integer array.
 	 */
 	public class lab2694 {
-	    /**
-	     * Performs a linear search on the given array to find the specified key.
-	     *
-	     * @param arr the array to be searched
-	     * @param key the search key
-	     * @return the index of the key if found, -1 otherwise
-	     */
+	    
 	    public static int linearSearch(int[] arr, int key) {
 	        for (int i = 0; i < arr.length; i++) {
 	            if (arr[i] == key) {
@@ -22,13 +16,7 @@
 	        return -1; // Return -1 if the key is not found
 	    }
 
-	    /**
-	     * Performs an interpolation search on the given sorted array to find the specified key.
-	     *
-	     * @param arr the sorted array to be searched
-	     * @param key the search key
-	     * @return the index of the key if found, -1 otherwise
-	     */
+	   
 	    public static int interpolationSearch(int[] arr, int key) {
 	        int low = 0;
 	        int high = arr.length - 1;
@@ -57,14 +45,7 @@
 	        return -1;
 	    }
 
-	    /**
-	     * Performs an improved linear search on the given array to find the specified key.
-	     * This implementation incorporates an early termination condition for better performance.
-	     *
-	     * @param arr the array to be searched
-	     * @param key the search key
-	     * @return the index of the key if found, -1 otherwise
-	     */
+	   
 	    public static int linearSearchImproved(int[] arr, int key) {
 	        int lastIndex = arr.length - 1;
 	        int lastElement = arr[lastIndex];
@@ -87,11 +68,6 @@
 	        return -1; // Return -1 if the key is not found
 	    }
 
-	    /**
-	     * Main method to demonstrate the linear search and interpolation search algorithms.
-	     *
-	     * @param args command line arguments (not used)
-	     */
 	    public static void main(String[] args) {
 	        Scanner scanner = new Scanner(System.in);
 
